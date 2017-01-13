00c826373b279fb341d80f4e5627619beca08a48
Derek Rodriguez <drod2169@gmail.com>
2017-01-13 08:31:37 -0500

    Hello World!

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+//
+//  helloworld.cpp
+//  
+//
+//  Created by Derek Rodriguez on 12/5/16.
+//
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string hello = "hello world!";
+    cout << "String: " << hello << endl;
+    
+    return 0;
+    
+    
+}
