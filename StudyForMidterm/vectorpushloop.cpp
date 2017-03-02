cdf4b5a8f5e7c311952f83a8a9012fe0fe1bb33f
Derek Rodriguez <drod2169@gmail.com>
2017-03-02 06:02:49 -0500

    Add pop_back function to clear the vector

--- StudyForMidterm/vectorpushloop.cpp
+++ StudyForMidterm/vectorpushloop.cpp
@@ -1,6 +1,6 @@
 //
 //  vectorpushloop.cpp
-//  
+//
 //
 //  Created by Derek Rodriguez on 3/2/17.
 //
@@ -13,14 +13,15 @@ using namespace std;
 int main() {
     
     vector <int> vec;
- 
+    
     int j;
     
     
+    cout << "Reading in integers to vector." << endl;
     
     for (int i = 0; i < 5; i++) {
         
-        cout << "Enter a variable." << endl;
+        cout << "Enter an integer." << endl;
         
         cin >> j;
         
@@ -29,10 +30,23 @@ int main() {
     }
     
     cout << endl;
+    cout << "Values in the vector: " << endl;
+    // Range based for loop loops over VALUES, not indices.
     
     for (int vecs : vec) {
         
         cout << vecs << endl;
+        
+    }
+    
+    cout << "Removing items in the vector: " << endl;
+    int i = 0;
+    
+    while (!vec.empty()) {
+        
+        cout << "Removing value in vector of size " << vec.size() << endl;
+        
+        vec.pop_back();
         
     }
     
